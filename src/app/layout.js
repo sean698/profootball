1a768973c17e0bc4a28948f808d096be4b15669f
Crowreport <robcroley@gmail.com>
2025-04-23 18:26:31 -0500

    Hello world

--- src/app/layout.js
+++ src/app/layout.js
@@ -10,7 +10,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`antialiased-2`}
+        className={`antialiased-2me`}
       >
         {children}
       </body>
